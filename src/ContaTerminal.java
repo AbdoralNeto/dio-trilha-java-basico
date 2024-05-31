83486f22c9d17ce8356ed54aae2356af1aaf59d6
AbdoralNeto <milgarden26@proton.me>
2024-05-31 13:57:00 -0300

    criação de conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importa classe scanner
+
+        //Exibir as mensagens para o usuário
+
+        //Obter pela scanner os valores digitados no terminal
+        
+        //Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.err.println("Digite o número da Agencia: ");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        double saldoConta = 100.0;
+        System.out.println("-----------------------------------------------------");
+        System.out.println();
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta no nosso banco.");
+        System.out.println("Sua Agencia é: "+numeroAgencia);
+        System.out.println("Sua Conta é: "+numeroConta);
+
+        if (saldoConta <= 0.0){
+            System.out.println("Seu saldo é: R$ "+saldoConta+", você não tem saldo disponivel para saque!");
+        }else{
+            System.out.println("Seu saldo é: R$ "+saldoConta+", seu saldo já está disponivel para saque!");
+        }
     }
 }
